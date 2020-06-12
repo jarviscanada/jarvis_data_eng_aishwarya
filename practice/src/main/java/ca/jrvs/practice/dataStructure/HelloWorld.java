15b99565da2e327094c4958933560dfd03165c5a
Aishwarya Parackal <aishwarya.parackal34@gmail.com>
2020-06-12 17:17:45 +0000

    Initial practice project

---
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/HelloWorld.java
@@ -0,0 +1,12 @@
+package ca.jrvs.practice.dataStructure;
+
+import java.util.List;
+
+public class HelloWorld {
+
+  // Your program begins with a call to main().
+  // Prints "Hello, World" to the terminal window.
+  public static void main(String args[]) {
+    System.out.println("Hello, World");
+  }
+}
