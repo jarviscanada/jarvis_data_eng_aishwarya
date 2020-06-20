9173d98cade95d21847028e1819f373b6d73e4c1
Aishwarya Parackal <aishwarya.parackal34@gmail.com>
2020-06-20 15:02:08 +0000

    removed comments and added logger

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Entities.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Entities.java
@@ -27,22 +27,6 @@ public class Entities {
     return hashtag;
   }
 
-  //private List<Hashtag> getHashtagList() {
-  // Pattern pattern = Pattern.compile("(#\\w+)\\b", Pattern.CASE_INSENSITIVE);
-  // Matcher matcher = pattern.matcher(getTweet());
-  // this.hashtag = new ArrayList<>();
-  // while (matcher.find()) {
-  //  Hashtag hashtag = new Hashtag();
-  //  List<Integer> index = new ArrayList<>();
-  //  index.add(this.tweet.indexOf(matcher.group(1)));
-  //  index.add((index.get(0) + matcher.group(1).length()) - 1);
-  //  hashtag.setIndices(index);
-  // hashtag.setText(matcher.group(1).substring(1));
-  // this.hashtag.add(hashtag);
-  // }
-  // return this.hashtag;
-  //}
-
   @JsonProperty("hashtags")
   public void setHashtag(List<Hashtag> hashtag) {
     this.hashtag = hashtag;
@@ -52,22 +36,6 @@ public class Entities {
   public List<UserMention> getUserMention() {
     return userMention;
   }
-
-  //public List<UserMention> getUserMentionList() {
-  //  Pattern pattern = Pattern.compile("(@\\w+)\\b", Pattern.CASE_INSENSITIVE);
-  //  Matcher matcher = pattern.matcher(this.tweet);
-  //  this.userMention=new ArrayList<>();
-  //  while (matcher.find()) {
-  //   UserMention userMention = new UserMention();
-  //   List<Integer> index = new ArrayList<>();
-  //   index.add(this.tweet.indexOf(matcher.group(1)));
-  //   index.add((index.get(0) + matcher.group(1).length()));
-  //  userMention.setIndices(index);
-  //  userMention.setScreenName(matcher.group(1).substring(1));
-  //  this.userMention.add(userMention);
-  //}
-  //return this.userMention;
-  //}
 
   @JsonProperty("user_mentions")
   public void setUserMention(List<UserMention> userMention) {
--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelperTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelperTest.java
@@ -6,8 +6,11 @@ import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TwitterHttpHelperTest {
+  final Logger logger = LoggerFactory.getLogger(TwitterHttpHelper.class);
 
   private String consumerKey;
   private String consumerSecret;
@@ -15,39 +18,27 @@ public class TwitterHttpHelperTest {
   private String tokenSecret;
   HttpHelper httpHelper;
 
-  //@Before
-  //public void setUp() {
-  //  consumerKey = System.getenv("consumerKey");
-  //  consumerSecret= System.getenv("consumerSecret");
-  //  accessToken = System.getenv("accessToken");
-  //  tokenSecret = System.getenv("tokenSecret");
-  //  HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret,
-  //      accessToken, tokenSecret);
-  //}
+  @Before
+  public void setUp() {
+    consumerKey = System.getenv("consumerKey");
+    consumerSecret= System.getenv("consumerSecret");
+    accessToken = System.getenv("accessToken");
+    tokenSecret = System.getenv("tokenSecret");
+    this.httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret,
+        accessToken, tokenSecret);
+  }
 
   @Test
   public void httpPost() throws Exception {
-    consumerKey = System.getenv("consumerKey");
-    consumerSecret = System.getenv("consumerSecret");
-    accessToken = System.getenv("accessToken");
-    tokenSecret = System.getenv("tokenSecret");
-    HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret,
-        accessToken, tokenSecret);
     HttpResponse response = httpHelper.httpPost(new URI(
         "https://api.twitter.com/1.1/statuses/update.json?status=Hello_tweet_test_2"));
-    System.out.println(EntityUtils.toString(response.getEntity()));
+    logger.info(EntityUtils.toString(response.getEntity()));
   }
 
   @Test
   public void httpGet() throws Exception {
-    consumerKey = System.getenv("consumerKey");
-    consumerSecret = System.getenv("consumerSecret");
-    accessToken = System.getenv("accessToken");
-    tokenSecret = System.getenv("tokenSecret");
-    HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret,
-        accessToken, tokenSecret);
     HttpResponse response = httpHelper.httpGet(
         new URI("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=AParackal"));
-    System.out.println(EntityUtils.toString(response.getEntity()));
+    logger.info(EntityUtils.toString(response.getEntity()));
   }
 }
