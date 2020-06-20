cff5f5b235a3eb381ef48549f00755e6b77a4eb8
Aishwarya Parackal <aishwarya.parackal34@gmail.com>
2020-06-20 23:24:18 +0000

    updated all controller files

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterCLIApp.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterCLIApp.java
@@ -1,5 +1,79 @@
 package ca.jrvs.apps.twitter;
 
+import ca.jrvs.apps.twitter.controller.Controller;
+import ca.jrvs.apps.twitter.controller.TwitterController;
+import ca.jrvs.apps.twitter.dao.CrdDao;
+import ca.jrvs.apps.twitter.dao.TwitterDao;
+import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
+import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
+import ca.jrvs.apps.twitter.example.JsonParser;
+import ca.jrvs.apps.twitter.model.Tweet;
+import ca.jrvs.apps.twitter.service.Service;
+import ca.jrvs.apps.twitter.service.TwitterService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public class TwitterCLIApp {
 
+  public static final String USAGE = "USAGE: TwitterCLI post|show|delete [options]";
+
+  private Controller controller;
+
+  //@Autowired
+  public TwitterCLIApp(Controller controller) {
+    this.controller = controller;
+  }
+
+  /**
+   * It parses args and calls the controller methods. It also prints tweet(s) returned by controller
+   * methods.
+   *
+   * @param args user inputs
+   */
+  public void run(String[] args) {
+    if (args.length == 0) {
+      throw new IllegalArgumentException(USAGE);
+    }
+    switch (args[0].toLowerCase()) {
+      case "post":
+        printTweet(controller.postTweet(args));
+        break;
+      case "show":
+        printTweet(controller.showTweet(args));
+        break;
+      case "delete":
+        controller.deleteTweet(args).forEach(this::printTweet);
+        break;
+      default:
+        throw new IllegalArgumentException(USAGE);
+    }
+  }
+
+  private void printTweet(Tweet tweet) {
+    try {
+      System.out.println(JsonParser.toJson(tweet, true, false));
+    } catch (JsonProcessingException ex) {
+      throw new RuntimeException("Unable to convert tweet object to string", ex);
+    }
+  }
+
+  /**
+   * Declare and instantiate all components. Call run method which calls controller methods and
+   * print tweet(s).
+   *
+   * @param args user inputs
+   */
+  public static void main(String[] args) {
+    String consumerKey = System.getenv("consumerKey");
+    String consumerSecret = System.getenv("consumerSecret");
+    String accessToken = System.getenv("accessToken");
+    String tokenSecret = System.getenv("tokenSecret");
+    HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken,
+        tokenSecret);
+    CrdDao dao = new TwitterDao(httpHelper);
+    Service service = new TwitterService(dao);
+    Controller controller = new TwitterController(service);
+    TwitterCLIApp twitterCLIApp = new TwitterCLIApp(controller);
+
+    twitterCLIApp.run(args);
+  }
 }
--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/controller/TwitterControllerUnitTest/TwitterControllerUnitTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/controller/TwitterControllerUnitTest/TwitterControllerUnitTest.java
@@ -1,5 +1,48 @@
 package ca.jrvs.apps.twitter.dao.controller.TwitterControllerUnitTest;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import ca.jrvs.apps.twitter.controller.TwitterController;
+import ca.jrvs.apps.twitter.model.Tweet;
+import ca.jrvs.apps.twitter.service.TwitterService;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TwitterControllerUnitTest {
+
+  @Mock
+  TwitterService service;
+
+  @InjectMocks
+  TwitterController controller;
+
+  @Test
+  public void postTweet(){
+    when(service.postTweet(any())).thenReturn(new Tweet());
+    String[] userInputs = {"post", "Hey controller testing with Unit test", "50.0:1.0"};
+    controller.postTweet(userInputs);
+  }
+
+  @Test
+  public void showTweet(){
+    when(service.showTweet(anyString(),any())).thenReturn(new Tweet());
+    String[] user_showinputs = {"show", "123"};
+    controller.showTweet(user_showinputs);
+  }
+
+  @Test
+  public void deleteTest(){
+    when(service.deleteTweets(any())).thenReturn(new ArrayList<Tweet>());
+    String[] user_deleteinputs = {"delete", "1,2,3"};
+    controller.deleteTweet(user_deleteinputs);
+  }
 
 }
